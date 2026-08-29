951dfd9c75073a250b745ac5924f9921180e54db
sondos tarik <sondos_tarik@gmail.com>
2026-08-29 06:23:28 -1000

    feat: complete user model

---
+++ src/models/User.model.js
@@ -0,0 +1,107 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcryptjs = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            validate(val) {
+                if (!validator.isEmail(val)) {
+                    throw new Error("Email is INVALID");
+                }
+            },
+        },
+
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 6,
+            select: false,
+        },
+
+        phone: {
+            type: String,
+            trim: true,
+        },
+
+        avatar: {
+            type: String,
+            trim: true,
+        },
+
+        role: {
+            type: String,
+            enum: ["admin", "customer"],
+            default: "customer",
+        },
+
+        addresses: [
+            {
+                type: mongoose.Schema.Types.Mixed,
+            },
+        ],
+
+        wishlist: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+            },
+        ],
+
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+
+        resetPasswordToken: {
+            type: String,
+        },
+
+        resetPasswordExpire: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Hash Password
+
+userSchema.pre("save", async function () {
+    const user = this;
+
+    if (user.isModified("password")) {
+        user.password = await bcryptjs.hash(user.password, 10);
+    }
+});
+
+////////////////////////////////////////////////////////////////////////////////////////
+// Compare Password
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    const user = this;
+
+    return await bcryptjs.compare(enteredPassword, user.password);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+

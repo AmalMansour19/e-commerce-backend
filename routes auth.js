fb461b4c39c71818d97003e23bb77c895b8072f1
sammoh356 <sammoh356@gmail.com>
2026-09-07 10:01:37 +0300

    Update routes auth.js

--- routes auth.js
+++ routes auth.js
@@ -1,6 +1,25 @@
-const { login, getMe, logout } = require('../controllers/authController');
-const { protect } = require('../middleware/protect');
+const express = require('express');
+const router = express.Router();
+const { login, getMe, logout } = require('./controllers/authController');
+const { protect } = require('./middleware/protect');
+const Joi = require('joi');
 
-router.post('/auth/login', login);
+const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+const validate = (schema) => (req, res, next) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  next();
+};
+
+
+router.post('/auth/login', validate(loginSchema), login);
 router.get('/auth/me', protect, getMe);
 router.post('/auth/logout', protect, logout);
+
+module.exports = router;

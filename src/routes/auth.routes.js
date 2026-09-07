c269f2e70b26a26cb83b84cbb4070defb06c491e
sammoh356 <sammoh356@gmail.com>
2026-09-07 18:47:52 +0300

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,16 @@
 import express from "express";
-import {
-  sendOtp,
-  verifyOtp,
-} from "../controllers/auth.controller.js";
+import { sendOtp, verifyOtp, login, getMe, logout } from "../controllers/auth.controller.js";
+import auth from "../middleware/auth.middleware.js";
+import { loginSchema } from "../validation/auth.validation.js";
+import validate from "../middleware/validate.middleware.js";
 
 const router = express.Router();
 
 router.post("/register/send-otp", sendOtp);
 router.post("/verify-otp", verifyOtp);
+
+router.post("/login", validate(loginSchema), login);
+router.get("/me", auth, getMe);
+router.post("/logout", auth, logout);
 
 export default router;

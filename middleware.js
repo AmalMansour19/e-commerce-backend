a68000a5ba8e1b9b00c79d545b4262bdf161c224
sammoh356 <sammoh356@gmail.com>
2026-09-07 09:55:45 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,18 +1,28 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User.model');
+import jwt from "jsonwebtoken";
+import User from "../models/User.model.js";
 
 const protect = async (req, res, next) => {
-  let token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(401).json({ message: 'Not authorized' });
-
   try {
-    const decode = jwt.verify(token, process.env.JWT_SECRET);
-   
-    req.user = await User.findById(decode._id);
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader ||!authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Not authorized, no token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded._id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
-    res.status(401).json({ message: 'Token failed' });
+    res.status(401).json({ message: "Token failed" });
   }
 };
 
-module.exports = { protect };
+export default protect;

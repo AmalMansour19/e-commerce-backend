03ab0700584e04950aead7f3ce4060e07f37f6c7
sammoh356 <sammoh356@gmail.com>
2026-09-07 21:08:59 +0300

    Delete src/utils/generatetoken.js

--- src/utils/generatetoken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) => {
-  return jwt.sign({ _id:id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-};
-
-export default generateToken;

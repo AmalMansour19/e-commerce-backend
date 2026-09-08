7b784981b0cad6e6b1b0671088b9a825ef04d5c9
EngMohammedyasser <mhmdyasrmohammedyasser@gmail.com>
2026-09-08 10:33:45 +0300

    feat: connect payment routes

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import wishlistRoutes from "./routes/wishlist.routes.js";
 import orderRoutes from "./routes/order.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 
 //import adminRoutes from "./routes/admin.routes.js";
 
@@ -32,7 +33,7 @@ app.use("/products", productRoutes);
 app.use("/carts", auth, cartRoutes);
 app.use("/wishlists", wishlistRoutes);
 app.use("/orders", orderRoutes);
-
+app.use("/payments", paymentRoutes);
 //app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {

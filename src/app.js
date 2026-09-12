bf3d00c94aed4357dc827a2671f5802560705df7
Wedad Noman <walqorashee@gmail.com>
2026-09-11 17:12:19 -0700

    feat: add admin dashboard and orders

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
 import wishlistRoutes from "./routes/wishlist.routes.js";
 import orderRoutes from "./routes/order.routes.js";
-//import adminRoutes from "./routes/admin.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 
 const app = express();
 
@@ -21,7 +21,7 @@ app.use("/products", productRoutes);
 app.use("/carts", auth, cartRoutes);
 app.use("/wishlists", wishlistRoutes);
 app.use("/orders", orderRoutes);
-//app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.json({

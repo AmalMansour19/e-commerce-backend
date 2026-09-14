56a25faf8d04e55f9b9672acee58a7bc40321512
Amal Mansour <elhayaa28@gmail.com>
2026-09-14 16:05:32 +0300

    Add product reference to Order model items

--- src/models/Order.model.js
+++ src/models/Order.model.js
@@ -9,6 +9,11 @@ user:{
 items:{
     type:[
      {
+         product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
         name:{
             type:String,
             required:true
